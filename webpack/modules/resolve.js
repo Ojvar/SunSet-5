7826e716825118591ef24057a7cca0c9eccd6194
Ojvar <ojvar@localhost.localdomain>
2020-11-18 13:27:14 +0330

    Update webpac

--- webpack/modules/resolve.js
+++ webpack/modules/resolve.js
@@ -6,8 +6,16 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
  * Export
  */
 module.exports = (env = {}) => {
+  const Path = require("path");
+
   return {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".vue", ".pug", ".json"],
     plugins: [new TsconfigPathsPlugin({})],
+    alias: {
+      "@FONTS": Path.resolve("src/frontend/fonts"),
+      "@IMAGES": Path.resolve("src/frontend/images"),
+      "@SCRIPTS": Path.resolve("src/frontend/scripts"),
+      "@STYLES": Path.resolve("src/frontend/styles"),
+    },
   };
 };

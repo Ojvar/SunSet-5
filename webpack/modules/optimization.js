7d462aa033018b105c18195a63fd237ea7ec6dfc
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-04-24 11:19:50 +0430

    Update webpack optimization

--- webpack/modules/optimization.js
+++ webpack/modules/optimization.js
@@ -3,7 +3,6 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = () => ({
     removeEmptyChunks: true,
-
     minimize: !Global.devMode,
     minimizer: [
         new TerserPlugin({
@@ -13,7 +12,18 @@ module.exports = () => ({
 
     splitChunks: {
         chunks: "all",
+        name(module) {
+            let packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            );
 
+            if ((packageName || []).length < 2) {
+                return null;
+            }
+            packageName = packageName[1];
+
+            return `chunks/${packageName.replace("@", "")}`;
+        },
         cacheGroups: {
             vue: {
                 test: /[\\/]node_modules[\\/]vue/i,
@@ -21,30 +31,40 @@ module.exports = () => ({
                 chunks: "all",
                 priority: 100,
             },
+
             buefy: {
                 test: /[\\/]node_modules[\\/]buefy/i,
                 name: "chunks/buefy",
                 chunks: "all",
-                priority: 90,
+                priority: 100,
             },
+
             validatorjs: {
                 test: /[\\/]node_modules[\\/]validatorjs/i,
                 name: "chunks/validatorjs",
                 chunks: "all",
-                priority: 80,
+                priority: 100,
             },
+
             axios: {
                 test: /[\\/]node_modules[\\/]axios/i,
                 name: "chunks/axios",
                 chunks: "all",
-                priority: 70,
+                priority: 100,
             },
 
-            default: {
-                test: /[\\/]node_modules[\\/]/i,
-                chunks: "all",
-                priority: -100,
-            },
+            //     default: {
+            //         test: /[\\/]node_modules[\\/]/i,
+            //         name(module) {
+            //             const packageName = module.context.match(
+            //                 /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            //             )[1];
+
+            //             return `chunks/${packageName.replace("@", "")}`;
+            //         },
+
+            //         ...groupsOptions,
+            //     },
         },
     },
 });

9de76e178d0b1d4c96fa2006852ddb7ed037a8bf
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-05-02 18:19:16 +0430

    Update route helper

--- resources/scripts/helpers/route-helper.ts
+++ resources/scripts/helpers/route-helper.ts
@@ -1,26 +1,44 @@
+import { BASE_URL } from "@CONFIGS/frontend/server";
 import { applyArguments } from "@CORE/helpers/route-helper";
 import { Hash } from "@Lib/types/hash-type";
-
-const BASE_ROUTE = `http://localhost:8085`;
 
 /**
  * Export routes
  */
 export const routes: Hash<string> = {
     /* GET */
-    "auth.login": `${BASE_ROUTE}/auth/login`,
-    "auth.logout": `${BASE_ROUTE}/auth/logout`,
-    "auth.google": `${BASE_ROUTE}/auth/google`,
+    "accounts.accounts": "accounts/accounts",
+    "accounts.load-exchange-list": "accounts/loadExchangeList",
+    "auth.google": "auth/google",
+    "auth.login": "auth/login",
+    "auth.logout": "auth/logout",
 
     /* POST */
-    "auth.attempt-to-login": `${BASE_ROUTE}/auth/login`,
+    "accounts.reg-exchange-accounte": "accounts/regExchangeAccounte",
+    "auth.attempt-to-login": "auth/login",
+    "auth.new-user-Activation-code": "auth/newUserActivationCode",
+    "auth.register-new-user": "auth/registerNewUser",
+    "auth.resend-new-user-Activation-code": "auth/resendNewUserActivationCode",
+    "auth.resend-reset-pwd-Activation-code":
+        "auth/resendResetPwdActivationCode",
+    "auth.reset-password": "auth/resetPassword",
+    "auth.reset-pwd-activation-code": "auth/resetPwdActivationCode",
 };
 
 /**
  * Get route with applied args
- * @param route {string} Route name
+ * @param routeName {string} Route name
  * @param args {any} Arguments
+ * @param queryString {any} Arguments
  */
-export function getRoute(route: string, args: any = {}): string {
-    return applyArguments(routes[route] || route, args);
+export function getRoute(
+    routeName: string,
+    args: any = {},
+    queryString: any = {}
+): string {
+    let route = routes[routeName]
+        ? `${BASE_URL}/${routes[routeName]}`
+        : routeName;
+
+    return applyArguments(route, args, queryString);
 }
